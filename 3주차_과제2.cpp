acb5b5ceee5106c0e3b5ee61485a6621866058cc
bleu05 <81405678+bleu05@users.noreply.github.com>
2021-08-13 21:56:54 +0900

    Add files via upload

---
+++ 3주차_과제2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+long long a[90];
+
+long long fibonacci(int n) {
+	a[0] = 0;
+	a[1] = 1;
+	for (int i = 0; i < n; i++) {
+		a[i + 2] = a[i + 1] + a[i];
+	}
+	return a[n];
+}
+
+int main() {
+	int n;
+	cin >> n;
+	cout << fibonacci(n);
+}
